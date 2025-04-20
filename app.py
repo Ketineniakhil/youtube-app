a8512795dd68beed6718c2597fb152f04148e574
Ketineniakhil <136882491+Ketineniakhil@users.noreply.github.com>
2025-04-20 14:00:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,6 +34,12 @@ import time
 nltk.download('punkt', quiet=True)
 nltk.download('stopwords', quiet=True)
 
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
 
 # Setup page config
